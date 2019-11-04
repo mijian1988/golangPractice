7710fe3ca2a3dddccdccfe4d0f4754f4a2747ac2
mijian1988 <31200645+mijian1988@users.noreply.github.com>
2019-11-04 15:44:02 +0800

    二分查找

--- binarySearch.go
+++ binarySearch.go
@@ -30,7 +30,7 @@ func main() {
 	arr := []int{21,32,12,33,34,34,87,24}
 	var n = len(arr)
 
-  // 冒泡排序
+  	// 冒泡排序
 	for i := 0; i <= n-1; i++ {
 		for j := i; j <= n-1; j++ {
 			if arr[i] > arr[j] {
